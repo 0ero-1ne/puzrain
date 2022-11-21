83392430891d3061bb4828f9516abee500d6c3a7
0ero-1ne <volikovd17@gmail.com>
2022-11-22 02:16:59 +0300

    [Header] Header component and styles

---
+++ src/components/header/Header.js
@@ -0,0 +1,29 @@
+import RegionSwitcher from './menu/RegionSwitcher/RegionSwitcher';
+import Menu from './menu/Menu/Menu';
+import Catalog from './main/Catalog/Catalog';
+import Searchbar from './main/Searchbar/Searchbar';
+import Links from './main/Links/Links';
+
+import './Header.css';
+
+export default function Header()
+{
+    return (
+        <header>
+            <div className="content">
+                <div className="header-row row-one">
+                    <RegionSwitcher />
+                    <Menu />
+                </div>
+                <div className="header-row row-two">
+                    <div className="header-logo">
+                        <img src="logo.svg" alt="Logo" width="80px" style={{display: 'block'}}/>
+                    </div>
+                    <Catalog />
+                    <Searchbar />
+                    <Links />
+                </div>
+            </div>
+        </header>
+    );
+}
