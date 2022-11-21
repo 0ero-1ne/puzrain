81090bb2a6c701cad5f777b5c55d519815db1ad1
0ero-1ne <volikovd17@gmail.com>
2022-11-22 02:19:09 +0300

    [Footer] Special words component

---
+++ src/components/footer/SpecialWords/SpecialWords.js
@@ -0,0 +1,12 @@
+import './SpecialWords.css';
+
+export default function SpecialWords() {
+    return (
+        <div className="footer-special">
+            <span id="special-words">
+                Some Special Words<br />
+                IDK :)
+            </span>
+        </div>
+    );
+}
