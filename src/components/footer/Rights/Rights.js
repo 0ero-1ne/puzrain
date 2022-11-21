cc1563f6a7e77b9ccddbfd47b0231da820ef290f
0ero-1ne <volikovd17@gmail.com>
2022-11-22 02:17:35 +0300

    [Footer] Rights component

---
+++ src/components/footer/Rights/Rights.js
@@ -0,0 +1,10 @@
+import './Rights.css';
+
+export default function Rights() {
+    return (
+        <div className="footer-rights">
+            <div className="footer-rights_block">&#169; Все права защищены<br />Puzrain 2010 - 2022</div>
+            <div className="footer-rights_block"><a>Политика безопасности<br />Условия использования</a></div>
+        </div>
+    );
+}
