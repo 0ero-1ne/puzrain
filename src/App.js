73486d8d4bc294d4fa161c0d7f92ef9c457225cc
0ero-1ne <volikovd17@gmail.com>
2022-11-27 21:32:57 +0300

    [App.js] Added BrowserRoutes component for paging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import About from './pages/about';
+import PLP from './pages/plp';
+import Main from './pages/main';
+
 import './App.css';
 
 function App() {
     return (
         <>
             <Header />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Main />}/>
+                    <Route path="/about" element={<About />}/>
+                </Routes>
+            </BrowserRouter>
             <Footer />
         </>
     );
