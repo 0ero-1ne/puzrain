abea4e66deac27dbbce2c3939bbe7dce916eb68a
0ero-1ne <volikovd17@gmail.com>
2022-11-27 21:36:22 +0300

    [Main] Added comment component

---
+++ src/components/main/Comment/Comment.js
@@ -0,0 +1,11 @@
+import './Comment.css';
+
+export default function Comment() {
+    return (
+        <div className="comments-comment">
+            <div className="comment-img"></div>
+            <div className="comment-text"></div>
+            <span className="comment-author">Дмитрий Воликов</span> 
+        </div>
+    );
+}
