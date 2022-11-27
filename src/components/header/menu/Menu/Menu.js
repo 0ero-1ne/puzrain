d89f0be9768a30dc748c168d4778ed9b5f36f7c5
0ero-1ne <volikovd17@gmail.com>
2022-11-27 21:33:56 +0300

    [Header Menu] Added path for about page

--- src/components/header/menu/Menu/Menu.js
+++ src/components/header/menu/Menu/Menu.js
@@ -3,7 +3,7 @@ import './Menu.css';
 export default function Menu() {
     return (
         <div className="header-menu">
-            <div className="header-menu_item"><a href="#">О нас</a></div>
+            <div className="header-menu_item"><a href="/about">О нас</a></div>
             <div className="header-menu_item"><a href="#">Доставка</a></div>
             <div className="header-menu_item"><a href="#">Контакты</a></div>
             <div className="header-menu_item"><a href="#">Адреса</a></div>
