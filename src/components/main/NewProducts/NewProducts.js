d96c9d35502c4d5475e6159983d110e18e54ac62
0ero-1ne <volikovd17@gmail.com>
2022-11-27 21:38:14 +0300

    [Main] Added new products component

---
+++ src/components/main/NewProducts/NewProducts.js
@@ -0,0 +1,21 @@
+import ProductElement from '../ProductElement/ProductElement';
+
+import './NewProducts.css';
+
+export default function NewProducts() {
+    let products = [];
+    for (let i = 0; i < 8; i++) {
+        products.push(
+            <ProductElement keyValue={"new-" + i} />
+        );
+    }
+
+    return (
+        <div className="main-newproducts">
+            <h2 className="newproducts-header">Новые товары</h2>
+            <div className="newproducts-list">
+                {products}
+            </div>
+        </div>
+    );
+}
