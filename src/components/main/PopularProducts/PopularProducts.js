632cafa2139a02c5f0eb19f96dede3e50f221b11
0ero-1ne <volikovd17@gmail.com>
2022-11-27 21:38:44 +0300

    [Main] Added popular products component

---
+++ src/components/main/PopularProducts/PopularProducts.js
@@ -0,0 +1,21 @@
+import ProductElement from '../ProductElement/ProductElement';
+
+import './PopularProducts.css';
+
+export default function PopularProducts() {
+    let products = [];
+    for (let i = 0; i < 8; i++) {
+        products.push(
+            <ProductElement keyValue={"popular-" + i} />
+        );
+    }
+
+    return (
+        <div className="main-popproducts">
+            <h2 className="popproducts-header">Популярные товары</h2>
+            <div className="popproducts-list">
+                {products}
+            </div>
+        </div>
+    );
+}
