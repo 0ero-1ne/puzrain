f6bf41cb60f89c5a4ccbdb61bec46921cd78fbeb
0ero-1ne <volikovd17@gmail.com>
2022-11-27 21:39:59 +0300

    [Main] Added special event component

---
+++ src/components/main/SpecialEvent/SpecialEvent.js
@@ -0,0 +1,13 @@
+import './SpecialEvent.css';
+
+export default function SpecialEvent() {
+    return (
+        <div className="main-event">
+            <div className="event-text">
+                <p>Новый Год с PUZRAIN</p>
+                <p>25 декабря - 01 января</p>
+                <p>Скидка 15% на все товары</p>
+            </div>
+        </div>  
+    );
+}
