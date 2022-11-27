d491852aa231b055536e6fa660d369f93c4a199f
0ero-1ne <volikovd17@gmail.com>
2022-11-27 21:37:35 +0300

    [Main] Added companies component

---
+++ src/components/main/Companies/Companies.js
@@ -0,0 +1,15 @@
+import './Companies.css';
+
+export default function Companies() {
+    return (
+        <div className="main-companies">
+            <img src="gan.png" alt="Gan" className="company-img" />
+            <img src="monster-go.png" alt="Monster Go" className="company-img" />
+            <img src="dayan.png" alt="DaYan" className="company-img" />
+            <img src="qiyi.png" alt="QiYi" className="company-img" />
+            <img src="moyu.png" alt="MoYu" className="company-img" />
+            <img src="dian-sheng.png" alt="Dian Sheng" className="company-img" />
+            <img src="yj.png" alt="YJ" className="company-img" /> 
+        </div>
+    );
+}
