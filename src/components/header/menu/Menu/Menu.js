76c4e79108cb3613213eace795261498f0d2907a
0ero-1ne <volikovd17@gmail.com>
2022-11-29 00:10:30 +0300

    [Header] Styles and menu changes

--- src/components/header/menu/Menu/Menu.js
+++ src/components/header/menu/Menu/Menu.js
@@ -2,12 +2,13 @@ import './Menu.css';
 
 export default function Menu() {
     return (
-        <div className="header-menu">
+        <nav className="header-menu">
             <div className="header-menu_item"><a href="/about">О нас</a></div>
-            <div className="header-menu_item"><a href="#">Доставка</a></div>
-            <div className="header-menu_item"><a href="#">Контакты</a></div>
-            <div className="header-menu_item"><a href="#">Адреса</a></div>
-            <div className="header-menu_item"><a href="#">Оплата</a></div>
-        </div>
+            <div className="header-menu_item"><a href="/reviews">Отзывы</a></div>
+            <div className="header-menu_item"><a href="/delivery">Доставка</a></div>
+            <div className="header-menu_item"><a href="/contacts">Контакты</a></div>
+            <div className="header-menu_item"><a href="/addresses">Адреса</a></div>
+            <div className="header-menu_item"><a href="/payment">Оплата</a></div>
+        </nav>
     );
 }
