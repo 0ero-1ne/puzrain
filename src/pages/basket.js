ac530f11b0ea58c4193439ceb6195c891af2f7b5
0ero-1ne <volikovd17@gmail.com>
2022-11-30 00:59:25 +0300

    [Basket] Added basket page

---
+++ src/pages/basket.js
@@ -0,0 +1,13 @@
+import '../components/main/Main.css';
+import Basket from '../components/basket/Basket';
+
+
+export default function BasketPage() {
+    return (
+        <main>
+            <div className="content">
+                <Basket />
+            </div>
+        </main>
+    );
+}
