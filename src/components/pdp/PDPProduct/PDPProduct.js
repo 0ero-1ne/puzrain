88930578f3d12c25f37cc86cfdbbf7fef31bbd76
0ero-1ne <volikovd17@gmail.com>
2022-12-05 01:22:02 +0300

    [PDP] Added product images component

---
+++ src/components/pdp/PDPProduct/PDPProduct.js
@@ -0,0 +1,20 @@
+import './PDPProduct.css';
+
+export default function PDPProduct() {
+    return (
+        <div className="main-pdp_product">
+            <img src="https://cdn.shopify.com/s/files/1/0270/0342/0758/articles/pasted_image_0_600x600_crop_center.png?v=1612781121" alt="Gan" />
+            <div className="pdp-product_images">
+                <div className="pdp-product_image">
+                    <img src="https://cdn.shopify.com/s/files/1/0270/0342/0758/articles/pasted_image_0_600x600_crop_center.png?v=1612781121" alt="Gan" />
+                </div>
+                <div className="pdp-product_image">
+                    <img src="https://cdn.shopify.com/s/files/1/0270/0342/0758/articles/pasted_image_0_600x600_crop_center.png?v=1612781121" alt="Gan" />
+                </div>
+                <div className="pdp-product_image">
+                    <img src="https://cdn.shopify.com/s/files/1/0270/0342/0758/articles/pasted_image_0_600x600_crop_center.png?v=1612781121" alt="Gan" />
+                </div>
+            </div>
+        </div>
+    );
+}
