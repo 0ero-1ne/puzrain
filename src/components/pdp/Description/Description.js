7f2040fdd4759fdf13ff618793a927fdaf8461ea
0ero-1ne <volikovd17@gmail.com>
2022-12-05 01:22:48 +0300

    [PDP] Added product description component

---
+++ src/components/pdp/Description/Description.js
@@ -0,0 +1,12 @@
+import './Description.css';
+
+export default function Description() {
+    return (
+        <div className="main-pdp_description">
+            <h2>Описание</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda provident, ad dolorum quis quod esse totam quam. In nostrum quaerat at hic! Enim maiores dolor quos, iste voluptas vitae qui?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit illo non tempora eius quibusdam quo voluptatem minima, dolor, placeat hic alias delectus iusto tenetur dolores, expedita enim ipsum nihil blanditiis!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, placeat error porro accusantium amet dolor soluta aliquam beatae nesciunt asperiores, perferendis earum recusandae? Minus dolorum et voluptatum, aperiam enim magnam.</p>
+        </div>
+    );
+}
