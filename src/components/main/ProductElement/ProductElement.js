8e001e870c5f1885cdcc812945690d9b632a677f
0ero-1ne <volikovd17@gmail.com>
2022-12-08 03:26:01 +0300

    [ProductElement] Some logic and markup fixes

--- src/components/main/ProductElement/ProductElement.js
+++ src/components/main/ProductElement/ProductElement.js
@@ -1,14 +1,22 @@
 import './ProductElement.css';
 
-export default function ProductElement() {
+export default function ProductElement(props) {
     return (
-        <div className="main-product">
-            <div className="product-image"></div>
-            <div className="product-description"></div>
+        <div className={props.className + " main-product"}>
+            <div className="product-image">
+                <img src={`images/${props.value.title}-1.png`} alt={props.value.title} />
+            </div>
+            <div className="product-description">
+                {props.value.title}
+            </div>
             <div className="product-control">
-                <div className="product-price"></div>
-                <div className="product-tobasket product-button"></div>
-                <div className="product-tofav product-button"></div>
+                <div className="product-price">{props.value.price} BYN</div>
+                <div className="product-tobasket product-button">
+                    <img src="basket.png" alt="To Basket" />
+                </div>
+                <div className="product-tofav product-button">
+                    <img src="fav.png" alt="To Fav" />
+                </div>
             </div>
         </div>
     );
