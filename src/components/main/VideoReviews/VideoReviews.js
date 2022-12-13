19881cf5901e8735d246a814bfe778877ea023f8
0ero-1ne <volikovd17@gmail.com>
2022-12-14 00:25:28 +0300

    [Main] Changed content and styles

--- src/components/main/VideoReviews/VideoReviews.js
+++ src/components/main/VideoReviews/VideoReviews.js
@@ -5,7 +5,7 @@ export default function VideoReviews() {
         <div className="main-videoreviews">
             <h2 className="video-header">Свежие видеобзоры</h2>
             <div className="videoreviews-videos">
-                <iframe width="490" height="270" src="https://www.youtube.com/embed/IR9N3MoKRYo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="490" height="270" src="https://www.youtube.com/embed/uWAxy1J9aWQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <iframe width="490" height="270" src="https://www.youtube.com/embed/RNTxbLmYVGI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
