85bed4d196266d4b7e266ed399fab6ff6fb67bd9
0ero-1ne <volikovd17@gmail.com>
2022-12-14 02:49:37 +0300

    [Basket card] Activating promo

--- src/components/basket/Card/Card.js
+++ src/components/basket/Card/Card.js
@@ -1,14 +1,50 @@
+import { useState } from 'react';
 import './Card.css';
 
+let statePrice = 0;
+
+function applyPromo(promos, sale, isPromo, setIsPromo) {
+    const promo = document.getElementById('promo').value;
+
+    if (promo === "") {
+        alert("Вы не ввели промокод");
+    } else {
+        if (promos.indexOf(promo) !== -1 && (!isPromo)) {
+            document.getElementById('promo_sale').innerHTML = sale;
+            setIsPromo(true);
+            document.getElementById('promo').value = "";
+        } else {
+            alert('Промокод не принят');
+            document.getElementById('promo').value = "";
+        }
+    }
+}
+
+function changeSale (sale, isPromo) {
+    if (isPromo) {
+        document.getElementById('promo_sale').innerHTML = sale;
+    }
+}
+
 export default function Card(props) {
+    const [isPromo, setIsPromo] = useState(false);
     const products = props.products;
-    let amountOfProducts = 0, price = 0, sale = 0;
+    const promos = ['puzrain', '0ero_1ne'];
+
+    let amountOfProducts = 0, price = 0;
 
     for (let i = 0; i < products.length; i++) {
         amountOfProducts += products[i].basketQty;
         price += products[i].basketQty * products[i].price;
     }
+
+    let sale = (Math.round(price / 100 * 10) * 100) / 100;
     
+    if (price !== statePrice) {
+        statePrice = price;
+        changeSale(sale, isPromo);
+    }
+
     return (
         <div className="basket-card">
             <div className="card-fullprice card-item">
@@ -17,13 +53,17 @@ export default function Card(props) {
             </div>
             <div className="card-sale card-item">
                 <div>Скидка</div>
-                <div className="sale-price">{sale} BYN</div>
+                <div className="sale-price"><span id="promo_sale">0</span> BYN</div>
+            </div>
+            <div className="card-amount card-item">
+                <div className="amount-text">Итого</div>
+                <div className="amount-price">{ isPromo ? price - sale : price } BYN</div>
             </div>
             <div className="card-promo card-item">
                 <div className="card-promo-field">
-                    <input type="text" placeholder="Введите промокод" />
+                    <input type="text" placeholder="Введите промокод" id="promo" />
                 </div>
-                <div className="card-promo-apply">
+                <div className="card-promo-apply" style={{cursor: 'pointer'}} onClick={() => applyPromo(promos, sale, isPromo, setIsPromo)}>
                     Принять
                 </div>
             </div>
