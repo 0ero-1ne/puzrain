6f6679b56892b2f242c9212fcbf6f12195b8e414
0ero-1ne <volikovd17@gmail.com>
2022-12-13 03:12:48 +0300

    [pages] Added reviews page with styles for it

---
+++ src/pages/reviews.js
@@ -0,0 +1,37 @@
+import { useState, useEffect} from 'react';
+import Comment from '../components/main/Comment/Comment';
+
+import '../components/main/Main.css';
+import './styles/page.css';
+
+export default function Reviews() {
+    const [reviews, setReviews] = useState();
+    let result = [];
+        
+    useEffect(() => {
+        fetch('http://localhost:3001/reviews')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+            .catch(error => window.location.pathname = "/");
+    }, []);
+
+    document.querySelector('title').innerText = "Puzrain - Отзывы";
+
+    if (reviews) {
+        result = reviews.map(review => {
+            return (
+                <Comment key={review._id} comment={review} />
+            );
+        });
+    }
+
+    if (reviews)
+    return (
+        <div className="content">
+            <h2 style={{fontWeight: 500, fontSize: 30, marginBottom: 20}} className="reviews-header">Отзывы о наших товарах</h2>
+            <div className="main-reviews">
+                {result}
+            </div>
+        </div>
+    );
+}
