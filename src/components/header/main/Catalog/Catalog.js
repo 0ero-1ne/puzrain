f3335d116f5617893823f60b8645d8418587ef4c
0ero-1ne <volikovd17@gmail.com>
2022-12-14 00:22:34 +0300

    [Header] Catalog page and nav links

--- src/components/header/main/Catalog/Catalog.js
+++ src/components/header/main/Catalog/Catalog.js
@@ -3,7 +3,7 @@ import './Catalog.css';
 export default function Catalog() {
     return(
         <div className="header-catalog">
-            <span id="header-catalog">☰ Каталог</span>
+            <span id="header-catalog"><a href="/catalog">Каталог</a></span>
         </div>
     );
 }
--- src/components/header/main/Links/Links.js
+++ src/components/header/main/Links/Links.js
@@ -4,7 +4,7 @@ export default function Links() {
     return (
         <div className="header-links">
             <div className="header-link login">
-                <a href="#"><img src="login.png" alt="Login" width="80px"/></a>
+                <a href="/login"><img src="login.png" alt="Login" width="80px"/></a>
             </div>
             <div className="header-link basket">
                 <a href="/basket"><img src="basket.png" alt="Login" width="80px"/></a>
